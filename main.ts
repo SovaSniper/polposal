76f22a861c9ff2551e5b2b441ac54d3c9004ed05
5208980 <z5208980@zmail.unsw.edu.au>
2024-09-19 12:59:50 +1000

    init

---
+++ main.ts
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv';
+
+import {
+    getClient,
+    isCorrectAddress,
+    isSufficient
+} from './lib/utils';
+import { openCampusCodex } from './lib/chains/open-campus-codex';
+
+dotenv.config();
+const hash = '0x262ec99b269235fbae0450aab044d57caec49e1714c42e903031faafe49b2aa6';
+const chain = openCampusCodex.id;
+
+(async () => {
+    const client = getClient(chain)
+
+    const chainId = await client.getChainId()
+    console.log("Chain: ", chainId)
+
+    const transaction = await client.getTransaction({ hash })
+    console.log("Block Number: ", transaction.blockNumber)
+
+    if (!isSufficient(transaction)) {
+        console.error("In valid staking amount")
+        return;
+    }
+
+    if (!isCorrectAddress(transaction, chainId.toString())) {
+        console.error("In valid staking amount")
+        return;
+    }
+
+    // Validate!
+})()
